bdbfc7ee2acb316862f939cae486c6876a32922c
castanea02 <ahrhk2000@gmail.com>
2024-03-09 21:44:42 +0900

    Review, Profile Page

---
+++ pages/api/reviews.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: {
+      createdBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  res.json({
+    ok: true,
+    reviews,
+  });
+}
+/**함수를 반환하는 함수 실행시 withHandler에서 반환된 함수를 NextJS에서 실행*/
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
+
+//("get", fn, false)의 형식보다 가독성을 높이기 위해
+//withHandler 함수에서 interface로 인자를 묶어 이름을 지정하여 인자값을 보냄
